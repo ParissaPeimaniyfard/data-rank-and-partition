02081b481094cd15f10ffefaeeb3ce7d021eab4b
Parissa <zahra.pmf@gmail.com>
2023-10-31 11:50:35 +0100

    first function  is set

--- queries.py
+++ queries.py
@@ -1,8 +1,41 @@
 # pylint:disable=C0111,C0103
+import sqlite3
+
+conn = sqlite3.connect('data/ecommerce.sqlite')
+db = conn.cursor()
+
 
 def order_rank_per_customer(db):
-    pass  # YOUR CODE HERE
+    query = '''
+    SELECT orders.OrderID, orders.CustomerID, orders.OrderDate,
+    RANK() OVER (
+            PARTITION BY orders.CustomerID
+            ORDER BY orders.OrderDate
+        ) AS order_rank
+    FROM Orders
+    '''
+    db.execute(query)
+    results = db.fetchall()
+    return results
 
 
 def order_cumulative_amount_per_customer(db):
-    pass  # YOUR CODE HERE
+
+    query = '''
+    SELECT orders.OrderID, orders.CustomerID, orders.OrderDate,
+    SUM(orderdetails.Quantity * orderdetails.UnitPrice) OVER (
+            PARTITION BY orders.CustomerID
+            ORDER BY orders.OrderDate
+        ) AS cumulative_amount
+    FROM  Orders
+    JOIN  OrderDetails
+        ON  orders.OrderID = orderdetails.OrderID
+    '''
+    db.execute(query)
+    results = db.fetchall()
+    return results
+
+
+
+#print(order_rank_per_customer(db))
+print(order_cumulative_amount_per_customer(db))
